3f40f8bd61ac4c5f4bec52f5aa67d2ecae4b6875
welldecode <wellingtonalfredo550@gmail.com>
2023-08-02 01:33:20 -0300

    adicionando arquivos finais

--- views/site/layout.php
+++ views/site/layout.php
@@ -9,11 +9,31 @@
   <link rel="stylesheet" href="public/css/libs.css">
   <link rel="stylesheet" href="public/css/style.css">
   <link href="https://cdn.jsdelivr.net/npm/remixicon@3.4.0/fonts/remixicon.css" rel="stylesheet">
+  <!-- Primary Meta Tags -->
+  <title>DevStep - Desenvolvimento Web</title>
+  <meta name="title" content="DevStep - Desenvolvimento Web" />
+  <meta name="description" content="Experiencias digitais por meio de sites memoráveis" />
+
+  <!-- Open Graph / Facebook -->
+  <meta property="og:type" content="website" />
+  <meta property="og:url" content="https://devstep.com.br" />
+  <meta property="og:title" content="DevStep - Desenvolvimento Web" />
+  <meta property="og:description" content="Experiencias digitais por meio de sites memoráveis" />
+  <meta property="og:image" content="https://devstep.com.br" />
+
+  <!-- Twitter -->
+  <meta property="twitter:card" content="summary_large_image" />
+  <meta property="twitter:url" content="https://devstep.com.br" />
+  <meta property="twitter:title" content="DevStep - Desenvolvimento Web" />
+  <meta property="twitter:description" content="Experiencias digitais por meio de sites memoráveis" />
+  <meta property="twitter:image" content="https://devstep.com.br" />
+
+  <!-- Meta Tags Generated with https://metatags.io -->
 </head>
 
 <body>
- 
-  <?php echo $this->viewContent(); ?>   
+
+  <?php echo $this->viewContent(); ?>
   <script src="public/js/libs.js"></script>
   <script src="public/js/main.js"></script>
 </body>
